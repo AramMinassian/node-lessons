64709716d489ffd78dd47cffc96d50b5d5e7c0ea
Aram Minasyan <aramminasyan@Arams-MacBook-Air.local>
2021-07-12 15:56:12 +0400

    task on nodjs events

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-process.nextTick(() => {
-  console.log("tick");
-});
-setImmediate(() => {
-  console.log("immediate");
-});
-setTimeout(() => {
-  console.log("timer");
-},0);
+let { EventEmitter } = require("events");
+const logger = new EventEmitter();
+
+logger.on("message", (msg) => {
+  console.log(`New message ${msg}`);
+  msgs.push(msg);
+})
+
+logger.on("login", (name) => {
+  console.log(`New user ${name}`)
+  users.push(name);
+})
+
+logger.on("getUsers", () => {
+  console.log("Logged users:")
+  users.forEach(user => {
+    console.log(user);
+  })
+})
+logger.on("getMessages", () => {
+  console.log("Messages:")
+  msgs.forEach(msg => {
+    console.log(msg);
+  })
+})
+
+let users = [];
+let msgs = [];
+
+logger.emit('message', "Hello World");
+logger.emit('login', "Aram");
+logger.emit("getUsers");
+logger.emit("getMessages");
+
+
+
+
+
+
