6ff54d2eee69d1c16156b8e2a1827c9533fbb39f
Aram Minasyan <aramminasyan@Arams-MacBook-Air.local>
2021-07-31 15:28:03 +0400

    homework-6

--- index.js
+++ index.js
@@ -1,49 +1,105 @@
-const minimist = require("minimist");
-const fs = require("fs");
-const path = require("path");
-const zlib = require("zlib");
+const express = require("express");
 
 
+// -------------- Local user data storage -------------- // 
 
-function createZip(newFolderName) {
+let users = [];
 
-  // ------ Getting folder path from argv ------ //
-  const argv = minimist(process.argv.slice(2));
-  const { folderPath } = argv;
-  if (!folderPath) {
-    return
-  }
+// -------------- Server implementation -------------- // 
 
-  try {
-    // ------ Checking if the path exists and readable ------ //
-    fs.accessSync(folderPath, fs.constants.F_OK | fs.constants.R_OK);
+const app = express();
+const port = 3000;
+app.use(express.json());
 
-    // ------ Creating an adjacent folder  ------ //
-    const dirPath = path.dirname(folderPath);
-    const newFolderPath = path.join(dirPath, newFolderName)
-    fs.mkdirSync(newFolderPath);
 
-    // ------ Creating zip files from specified folder and inserting them into the new folder  ------ //
-    const files = fs.readdirSync(folderPath);
+app.get("/users", getUsers)
 
-    files.forEach(file => {
-      const filePath = path.join(folderPath, file);
+app.get("/users/:id", checkUser, getUser)
 
-      const readFileStream = fs.createReadStream(filePath);
-      const writeFileStream = fs.createWriteStream(path.join(newFolderPath, file + ".gz"))
+app.post("/users", validateBody, createUser)
 
-      const gzip = zlib.createGzip();
+app.delete("/users/:id", checkUser, deleteUser)
 
-      readFileStream
-        .pipe(gzip)
-        .pipe(writeFileStream);
+app.put("/users/:id", checkUser, validateBody, updateUser)
 
-    })
-  } catch (error) {
-    console.log(error.message);
-  }
 
+app.listen(port, () => {
+  console.log(`app is listening on port: ${port}`)
+});
+
+
+// -------------- Simple id generator -------------- // 
+
+function idGenerator() {
+  return Math.random().toString(32).slice(2) + "-" + Math.random().toString(32).slice(2)
 }
 
-// ------ New folder name is provided via function argument. Random name genarator package can also be used here  ------ //
-createZip("random-folder2");
+// -------------- Middlewares -------------- // 
+
+
+// getting all users
+function getUsers(req, res) {
+  res.send(users);
+}
+
+// getting a user with its id
+function getUser(req, res) {
+  const user = users.find(user => user.id === req.params.id)
+  res.send(user);
+}
+
+//creating a new user
+function createUser(req, res) {
+  const { body } = req;
+  const newUser = {id: idGenerator(), ...body};
+  users.push(newUser);
+  res.status(201).json(newUser);
+}
+
+//updating a user
+function updateUser(req, res){
+  const { body } = req;
+  const id = req.params.id;
+  const updatedUser = { id, ...body};
+  users = users.map(user => {
+    if (user.id === updatedUser.id) return updatedUser;
+    return user;
+  });
+  res.json(updatedUser);
+}
+
+//deleting a user
+function deleteUser(req, res){
+  users = users.filter(user => user.id !== req.params.id)
+  res.json({ success: true });
+}
+
+// checking if the user with the specified id exists
+function checkUser(req, res, next){
+  const userExists = users.some(user => user.id === req.params.id);
+  if(!userExists){
+    return res.status(404).json({
+      message:`User with the specified id does not exist`  
+    });
+  }
+  next();
+}
+
+// a simple body validation for POST and PUT requests
+function validateBody(req, res, next) {
+  const { body } = req;
+  if (!body.age) {
+    return res.status(400).json({message: "Bad request: body must have age field"});
+  }
+  else if (!body.name) {
+    return res.status(400).json({message: "Bad request: body must have name field"});
+  }
+  else if (!body.gender) {
+    return res.status(400).json({message: "Bad request: body must have gender field"});
+  }
+  else if (!body.email) {
+    return res.status(400).json({message: "Bad request: body must have email field"});
+  }
+  next();
+}
+
